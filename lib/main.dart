4b4f2f7d2da7f59f8843beba02b507c1b8bd7480
220278cs03 <220278@akfauniversity.org>
2023-02-23 17:35:58 +0500

    added - blocListener

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Bloc Lesson',
       home: BlocProvider(
         create: (_) => CounterCubit(),
-        child: MyHomePage(title: 'Bloc Lesson'),
+        child: const MyHomePage(title: 'Bloc Lesson'),
       ),
     );
   }
@@ -39,93 +39,114 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     print("main build");
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              BlocBuilder<CounterCubit, MainState>(
-                buildWhen: (prev, current){
-                  return current.counter != prev.counter;
-                },
-                builder: (context, state) {
-                  print("bloc build");
-                  return Text(
-                   "${state.counter}"
-                  );
-                },
-              ),
-              BlocBuilder<CounterCubit, MainState>(
-                buildWhen: (prev, current){
-                  return current.name != prev.name;
-                },
-                builder: (context, state) {
-                  print("bloc build");
-                  return Text(
-                      state.name
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisAlignment: MainAxisAlignment.end,
+    return BlocListener<CounterCubit, MainState>(
+      listenWhen: (prev, current){
+        return prev.counter != current.counter || prev.name != current.name;
+      },
+  listener: (context, state) {
+    if(state.counter == 5){
+      showDialog(context: context, builder: (context){
+        return const AlertDialog(
+          title: Text("5"),
+        );
+      });
+    }
+    if(state.name == "Najot Ta'lim"){
+      showDialog(context: context, builder: (context){
+        return const AlertDialog(
+          title: Text("Najot Ta'lim"),
+        );
+      });
+    }
+  },
+  child: Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FloatingActionButton(
-              child: const Icon(Icons.add),
-              onPressed: () {
-                context.read<CounterCubit>().increment();
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocBuilder<CounterCubit, MainState>(
+              buildWhen: (prev, current){
+                return current.counter != prev.counter;
+              },
+              builder: (context, state) {
+                print("bloc build");
+                return Text(
+                 "${state.counter}"
+                );
               },
             ),
-            const SizedBox(height: 4),
-            FloatingActionButton(
-              child: const Icon(Icons.remove),
-              onPressed: () => context.read<CounterCubit>().decrement(),
-            ),
-            const SizedBox(height: 4),
-            FloatingActionButton(
-              child: const Icon(Icons.edit),
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (_) => AlertDialog(
-                      title: TextFormField(
-                        controller: name,
-                      ),
-                      actions: [
-                        ElevatedButton(
-                            onPressed: () {
-                              context.read<CounterCubit>().addName(name.text);
-                              Navigator.pop(context);
-                            },
-                            child: Text("Save"))
-                      ],
-                    ));
+            BlocBuilder<CounterCubit, MainState>(
+              buildWhen: (prev, current){
+                return current.name != prev.name;
               },
-            ),
-            const SizedBox(height: 4),
-            FloatingActionButton(
-              child: const Icon(Icons.ac_unit),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (_) => BlocProvider.value(
-                          value: BlocProvider.of<CounterCubit>(context),
-                          child: const OnePage(),
-                        )));
+              builder: (context, state) {
+                print("bloc build");
+                return Text(
+                    state.name
+                );
               },
             ),
           ],
         ),
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              context.read<CounterCubit>().increment();
+            },
+          ),
+          const SizedBox(height: 4),
+          FloatingActionButton(
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          ),
+          const SizedBox(height: 4),
+          FloatingActionButton(
+            child: const Icon(Icons.edit),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (_) => AlertDialog(
+                    title: TextFormField(
+                      controller: name,
+                    ),
+                    actions: [
+                      ElevatedButton(
+                          onPressed: () {
+                            context.read<CounterCubit>().addName(name.text);
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Save"))
+                    ],
+                  ));
+            },
+          ),
+          const SizedBox(height: 4),
+          FloatingActionButton(
+            child: const Icon(Icons.ac_unit),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (_) => BlocProvider.value(
+                        value: BlocProvider.of<CounterCubit>(context),
+                        child: const OnePage(),
+                      )));
+            },
+          ),
+        ],
+      ),
 
-      );}
+    ),
+);}
 }
